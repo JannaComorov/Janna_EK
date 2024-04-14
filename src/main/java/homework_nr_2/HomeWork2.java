74a3fa177faf5c44f42762910b7239bde5e229c3
janna <ek50986@gmal.com>
2024-04-14 23:47:17 +0300

    Added homework_nr_2

---
+++ src/main/java/homework_nr_2/HomeWork2.java
@@ -0,0 +1,32 @@
+package homework_nr_2;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        long currentWorldPopulation = 7811080815L;
+        int carsProducedThisYear = 55235358;
+        char firstLetterInAlphabet = 'A';
+        boolean javaIsAmazing = true;
+        double pi = 3.14159;
+
+        System.out.println("Current World Population: " + currentWorldPopulation);
+        System.out.println("Cars Produced This Year: " + carsProducedThisYear);
+        System.out.println("First Letter In Alphabet: " + firstLetterInAlphabet);
+        System.out.println("Java Is Amazing: " + javaIsAmazing);
+        System.out.println("Pi: " + pi);
+
+        double Price = 10.3;
+        double Tax = 107.30;
+        int Quantity = 10;
+        double Sum = 0;
+        System.out.println("Price: "+Price);
+        System.out.println("Tax: "+Tax);
+        System.out.println("Quantity: "+Quantity);
+        System.out.println("Sum: "+Sum);
+
+        double Total = Price+Tax*Quantity+Sum;
+        System.out.println("Total: "+Total);
+
+    }
+}
